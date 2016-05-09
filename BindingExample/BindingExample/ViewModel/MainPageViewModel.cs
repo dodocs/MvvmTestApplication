adba957a2955b7ab23954e3fa410d73e1ed41fba
Dominik Wojciekiewicz <dodocss@gmail.com>
2016-05-09 18:23:44 +0200

    Initial commit

---
+++ BindingExample/BindingExample/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,100 @@
+﻿using BindingExample.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingExample.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            Cars = new List<Car>();
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+            Cars.Add(new Car
+            {
+                Manufacturer = "Audi",
+                Model = "S3",
+                Color = "Silver",
+                Year = 2016
+            });
+
+            Vehicles = new ObservableCollection<string> { "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", "Audi", };
+        }
+        public List<Car> Cars { get; set; }
+
+        public ObservableCollection<string> Vehicles { get; set; }
+    }
+}
